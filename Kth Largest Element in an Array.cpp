7ad7946600496285fe55e34073ba61865522e8d4
sahilkr28 <piyushkr13579@gmail.com>
2026-02-23 15:31:08 +0530

    Heaps - Date 23 Feb

--- Kth Largest Element in an Array.cpp
+++ Kth Largest Element in an Array.cpp
@@ -24,34 +24,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
-private:
-    int partition(vector<int>& nums, int left, int right) {
-        int pivot = nums[right];
-        int i = left;
-        for (int j = left; j < right; j++) {
-            if (nums[j] > pivot) {
-                swap(nums[i], nums[j]);
-                i++;
-            }
-        }
-        swap(nums[i], nums[right]);
-        return i;
-    }
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        int left = 0;
-        int right = nums.size() - 1;
-
-        while (left <= right) {
-            int pivotIndex = partition(nums, left, right);
-            if (pivotIndex == k - 1) {
-                return nums[pivotIndex];
-            } else if (pivotIndex < k - 1) {
-                left = pivotIndex + 1;
-            } else {
-                right = pivotIndex - 1;
-            }
-        }
-        return -1; // This line should never be reached
+        nth_element(nums.begin(), nums.begin() + k - 1, nums.end(), greater<int>());
+        return nums[k - 1];
     }
 };
